c79eb8d74285963ccb24e6700afe209db1d2fc62
Yateng Geng <yatenggeng@gmail.com>
2023-02-12 15:51:31 -0500

    updated

--- src/atguigu/mThreadDriver.java
+++ src/atguigu/mThreadDriver.java
@@ -4,7 +4,8 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Scanner;
 
-public class mThreadDriver {
+public class
+mThreadDriver {
     public static void main(String[] args) {
         mThread mt = new mThread();
         Thread td = new Thread(mt);
